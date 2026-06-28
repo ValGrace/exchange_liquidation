32f4567e756ef0285b027674b44bfe5dda5c9609
ValGrace <gracevalerie1@gmail.com>
2026-06-28 19:10:27 +0300

    chore: update the subscription payload arg

--- scripts/main.py
+++ scripts/main.py
@@ -1,13 +1,13 @@
 import asyncio
 from socket_handler import websocket_handler
 from binance import parse_binance_liquidations, binance_liq_url
-from bybit import bybit_url, parse_bybit_message
-from okx import parse_okx_message, okx_url
+from bybit import bybit_url, parse_bybit_message, bybit_subscribe_payload
+from okx import parse_okx_message, okx_url, okx_subscribe_payload
 
 async def main():
     binance_task = websocket_handler("Binance exchange", binance_liq_url, None, parse_binance_liquidations)
-    bybit_task = websocket_handler("ByBit exchange", bybit_url, None, parse_bybit_message)
-    okx_task = websocket_handler("OKX exchange", okx_url, None, parse_okx_message)
+    bybit_task = websocket_handler("ByBit exchange", bybit_url, bybit_subscribe_payload, parse_bybit_message)
+    okx_task = websocket_handler("OKX exchange", okx_url, okx_subscribe_payload, parse_okx_message)
 
     try:
         results = await asyncio.gather(
