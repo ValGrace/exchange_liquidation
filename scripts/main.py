e4222dc63ed23bdc4900bc56c293bd8fba234b4f
ValGrace <gracevalerie1@gmail.com>
2026-06-28 18:14:39 +0300

    chore: import okx and bybit functions

--- scripts/main.py
+++ scripts/main.py
@@ -1,13 +1,19 @@
 import asyncio
 from socket_handler import websocket_handler
 from binance import parse_binance_liquidations, binance_liq_url
+from bybit import bybit_url, parse_bybit_message
+from okx import parse_okx_message, okx_url
 
 async def main():
-    binance_task = websocket_handler("Binance", binance_liq_url, None, parse_binance_liquidations)
+    binance_task = websocket_handler("Binance exchange", binance_liq_url, None, parse_binance_liquidations)
+    bybit_task = websocket_handler("ByBit exchange", bybit_url, None, parse_bybit_message)
+    okx_task = websocket_handler("OKX exchange", okx_url, None, parse_okx_message)
 
     try:
         results = await asyncio.gather(
             binance_task,
+            bybit_task,
+            okx_task,
             return_exceptions=True
         )
         print(results)
