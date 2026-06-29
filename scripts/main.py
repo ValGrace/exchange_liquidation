cdde1a314053a50bb378c8d133a088f78f399ce8
ValGrace <gracevalerie1@gmail.com>
2026-06-29 05:57:15 +0300

    feat: implement kafka producer

--- scripts/main.py
+++ scripts/main.py
@@ -4,15 +4,29 @@ from scripts.exchanges.binance import parse_binance_liquidations, BINANCE_WS_URL
 from scripts.exchanges.bybit import BYBIT_WS_URL, parse_bybit_message, bybit_subscribe_payload
 from scripts.exchanges.okx import parse_okx_message, OKX_WS_URL, okx_subscribe_payload
 from scripts.exchanges.kraken import parse_kraken_message, KRAKEN_WS_URL, kraken_subscribe_payload
-from scripts.coin_base import parse_coinbase_message, COINBASE_WS_URL, coinbase_subscribe_payload
+from scripts.exchanges.coin_base import parse_coinbase_message, COINBASE_WS_URL, coinbase_subscribe_payload
+from kafka import KafkaProducer
+import logging
 
+KAFKA_TOPIC = 'crypto_exchange_topic'
 
 async def main():
-    binance_task = websocket_handler("Binance exchange", BINANCE_WS_URL, None, parse_binance_liquidations)
-    bybit_task = websocket_handler("ByBit exchange", BYBIT_WS_URL, bybit_subscribe_payload, parse_bybit_message)
-    okx_task = websocket_handler("OKX exchange", OKX_WS_URL, okx_subscribe_payload, parse_okx_message)
-    kraken_task = websocket_handler("KRAKEN exchange", KRAKEN_WS_URL, kraken_subscribe_payload, parse_kraken_message)
-    coinbase_task = websocket_handler("Coinbase exchange", COINBASE_WS_URL, coinbase_subscribe_payload, parse_coinbase_message)
+    logging.info("Initializing high-throughput Kafka producer...")
+
+    k_producer = KafkaProducer(
+        bootstrap_servers = 'broker:9092',
+        api_version=(2, 3, 1),
+        enable_idempotence=True
+    )
+
+    await k_producer.start()
+    logging.info("✅ Kafka producer started successfully.")
+
+    binance_task = websocket_handler("Binance exchange", BINANCE_WS_URL, None, parse_binance_liquidations, k_producer, KAFKA_TOPIC)
+    bybit_task = websocket_handler("ByBit exchange", BYBIT_WS_URL, bybit_subscribe_payload, parse_bybit_message, k_producer, KAFKA_TOPIC)
+    okx_task = websocket_handler("OKX exchange", OKX_WS_URL, okx_subscribe_payload, parse_okx_message, k_producer, KAFKA_TOPIC)
+    kraken_task = websocket_handler("KRAKEN exchange", KRAKEN_WS_URL, kraken_subscribe_payload, parse_kraken_message, k_producer, KAFKA_TOPIC)
+    coinbase_task = websocket_handler("Coinbase exchange", COINBASE_WS_URL, coinbase_subscribe_payload, parse_coinbase_message, k_producer, KAFKA_TOPIC)
 
     try:
         results = await asyncio.gather(
@@ -24,7 +38,11 @@ async def main():
             return_exceptions=True
         )
         print(results)
-    except KeyboardInterrupt:
-        print("\nEngine shutting down manually")
+    finally:
+        # Guarantee cleanup on shut down
+        logging.info("Flushing and shutting down Kafka producer...")
+        await k_producer.stop()
+        logging.info("✅ Pipelines cleanly offline.")
 
-asyncio.run(main())
+if __name__ == "__main__":
+    asyncio.run(main())
